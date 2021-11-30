b2036436f6aacc20f802c14bc8a47a0e0a5ce6c5
MariyaDemy <Mariya.Demi373@yandex.ru>
2021-12-01 01:27:44 +0300

    add hm2 calculator task

--- hm_2/array.js
+++ hm_2/array.js
@@ -7,6 +7,28 @@ styles.unshift('Рэп', 'Регги');
 console.log(styles);
 
 // assignment 2
+// Создайте калькулятор для введённых значений
+// Напишите код, который:
+// Запрашивает по очереди значения при помощи prompt и сохраняет их в массиве.
+// Заканчивает ввод, как только посетитель введёт число 15.
+// При этом ноль 0 не должен заканчивать ввод, это разрешённое число.
+// Выводит сумму всех значений массива
+let values = [];
+for (let f = 0; f < 3; f++) {
+    let value = +prompt('Enter any numbers you like');
+    if (value === 15) {
+        values.push(value)
+        break;
+    }
+    else { values.push(value) }
+}
+let valuesSum = values.reduce((sum, item) => { return sum + item; });
+alert(valuesSum);
+
+
+
+
+
 
 
 
