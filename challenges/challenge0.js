191a793dff0db68af834c504b0edc9c0d8541439
MariyaDemy <Mariya.Demi373@yandex.ru>
2021-11-30 03:46:40 +0300

    add challenge0

---
+++ challenges/challenge0.js
@@ -0,0 +1,16 @@
+let fullName = {
+    "last name": 'Демидович',
+    "first name": 'Мария',
+    patronymic: 'Николаевна'
+}
+console.table({ fullName });
+
+console.group("Interesting Facts");
+console.log("люблю кино и мультфильмы");
+console.log("изучаю шведский язык");
+console.log("люблю поспать");
+console.groupEnd();
+
+
+
+
